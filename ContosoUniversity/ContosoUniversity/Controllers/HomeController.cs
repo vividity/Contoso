0258e01387150f03c743a4cb71bfe3b94d351a52
vividity <vividitystore@yahoo.ca>
2015-05-04 13:50:53 -0400

    Chapter 3

--- ContosoUniversity/ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/ContosoUniversity/Controllers/HomeController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ContosoUniversity.DAL;
+using ContosoUniversity.ViewModels;
 
 namespace ContosoUniversity.Controllers
 {
+    
     public class HomeController : Controller
     {
+        private SchoolContext db = new SchoolContext();
+
         public ActionResult Index()
         {
             return View();
@@ -15,10 +20,24 @@ namespace ContosoUniversity.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            IQueryable<EnrollmentDateGroup> data = from student in db.Students
+                                                   group student by student.EnrollmentDate into dateGroup
+                                                   select new EnrollmentDateGroup()
+                                                   {
+                                                       EnrollmentDate = dateGroup.Key,
+                                                       StudentCount = dateGroup.Count()
+                                                   };
 
-            return View();
+            return View(data.ToList());
         }
+
+
+        // I am not sure exactly where this should go.
+        protected override void Dispose(bool disposing)
+{
+ 	 db.Dispose();
+            base.Dispose(disposing);
+}
 
         public ActionResult Contact()
         {
